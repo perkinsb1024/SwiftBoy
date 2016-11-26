6a5ab6e2af45ea7370f856c08b84507ec80c5fc2
Ben Perkins <perkinsb1024@gmail.com>
2016-11-25 21:08:07 -0800

    Initial Commit

---
+++ SwiftBoy/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SwiftBoy
+//
+//  Created by Ben Perkins on 11/25/16.
+//  Copyright © 2016 perkinsb1024. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SwiftBoy/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SwiftBoy
+//
+//  Created by Ben Perkins on 11/25/16.
+//  Copyright © 2016 perkinsb1024. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
